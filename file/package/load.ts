a7ec3a809c07c5874d6961b93070af4c941fc612
roll <eskarev@gmail.com>
2025-05-20 14:00:48 +0100

    Implemented loadPackageFromFolder

--- file/package/load.ts
+++ file/package/load.ts
@@ -1,5 +1,6 @@
-export async function loadPackageFromFolder(props: {
-  path: string
-}) {
-  console.log(props)
+import { loadPackage } from "@dpkit/core"
+import { join } from "node:path"
+
+export async function loadPackageFromFolder(props: { path: string }) {
+  return loadPackage({ path: join(props.path, "datapackage.json") })
 }
