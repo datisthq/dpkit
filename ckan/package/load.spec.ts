e61545a224f455b900d869a79c9c3ef690b4d7c0
roll <eskarev@gmail.com>
2025-05-22 12:59:52 +0100

    Implemented useRecording

--- ckan/package/load.spec.ts
+++ ckan/package/load.spec.ts
@@ -1,12 +1,15 @@
 import { describe, expect, it } from "vitest"
-//import { loadPackageFromCkan } from "./load.js"
+import { loadPackageFromCkan } from "./load.js"
+import { useRecording } from "@dpkit/test"
 
 describe("loadPackageFromCkan", () => {
+  useRecording()
+
   it("should load a package", async () => {
     expect(true).toBe(true)
-    //const datapackage = await loadPackageFromCkan({
-    //  datasetUrl:
-    //    "https://data.nhm.ac.uk/dataset/join-the-dots-collection-level-descriptions",
-    //})
+    const datapackage = await loadPackageFromCkan({
+      datasetUrl:
+        "https://data.nhm.ac.uk/dataset/join-the-dots-collection-level-descriptions",
+    })
   })
 })
