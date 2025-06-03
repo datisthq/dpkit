882a571f949e70259697bf6711c812a3ece96a53
roll <eskarev@gmail.com>
2025-06-03 16:23:17 +0100

    Bootstrapped readCell tests

--- table/cell/read.ts
+++ table/cell/read.ts
@@ -1,5 +1,13 @@
 import type { Field } from "@dpkit/core"
+import { Series } from "nodejs-polars"
+import { convertFieldToPolars } from "../field/index.js"
 
 export function readCell(props: { cell: unknown; field: Field }) {
-  console.log(props)
+  const { cell, field } = props
+
+  const polarsField = convertFieldToPolars({ field })
+  const polarsSeries = Series(polarsField.name, [cell], polarsField.type)
+
+  const value = polarsSeries[0]
+  return value
 }
