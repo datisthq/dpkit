d0a3aab59cc897115e4352005592e135369ac0bc
roll <eskarev@gmail.com>
2025-06-03 17:41:08 +0100

    Removed readCell

--- table/cell/index.ts
+++
@@ -1,1 +0,0 @@
-export { readCell } from "./read.js"
--- table/column/types/integer.spec.ts
+++ table/column/types/integer.spec.ts
@@ -4,28 +4,26 @@ import { parseIntegerColumn } from "./integer.js"
 
 describe("parseIntegerColumn", () => {
   it.each([
-    {
-      description: "general",
-      cells: ["1", "2", "3", "3.1", "bad", ""],
-      values: [1, 2, 3, null, null, null],
-    },
-    {
-      description: "groupChar (,)",
-      groupChar: ",",
-      cells: ["1", "1,000", "1.000"],
-      values: [1, 1000, null],
-    },
-    {
-      description: "bareNumber (false)",
-      bareNumber: false,
-      cells: ["1", "1000", "$1000", "1,000"],
-      values: [1, 1000, 1000, null],
-    },
-  ])("$description", async ({ cells, values, ...rest }) => {
-    const column = Series("name", cells, DataType.Utf8)
-    const field = { name: "name", type: "integer", ...rest }
+    ["1", 1, {}],
+    ["2", 2, {}],
+    ["1000", 1000, {}],
+
+    ["", null, {}],
+    ["2.1", null, {}],
+    ["bad", null, {}],
+
+    ["1", 1, { groupChar: "," }],
+    ["1,000", 1000, { groupChar: "," }],
+
+    ["1", 1, { bareNumber: false }],
+    ["1000", 1000, { bareNumber: false }],
+    ["$1000", 1000, { bareNumber: false }],
+    ["1,000", null, { bareNumber: false }],
+  ])("$0 -> $1 $2", async (cell, value, options) => {
+    const column = Series("name", [cell], DataType.Utf8)
+    const field = { name: "name", type: "integer", ...options }
 
     // @ts-ignore
-    expect(parseIntegerColumn({ column, field }).toArray()).toEqual(values)
+    expect(parseIntegerColumn({ column, field }).toArray()).toEqual([value])
   })
 })
