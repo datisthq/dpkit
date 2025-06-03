15f56193b671a2642b51066f76c1236066ad2574
roll <eskarev@gmail.com>
2025-06-03 16:04:37 +0100

    Bootstrapped read/write cell

---
+++ table/cell/index.ts
@@ -0,0 +1,2 @@
+export { readCell } from "./read.js"
+export { writeCell } from "./write.js"
---
+++ table/cell/read.ts
@@ -0,0 +1,5 @@
+import type { Field } from "@dpkit/core"
+
+export function readCell(props: { cell: unknown; field: Field }) {
+  console.log(props)
+}
