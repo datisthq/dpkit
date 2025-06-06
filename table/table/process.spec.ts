f76d1e0545e7a6015b858ea41a97f7caf5dceafb
roll <eskarev@gmail.com>
2025-06-06 08:40:45 +0100

    Rebased process tests on it

--- table/table/process.spec.ts
+++ table/table/process.spec.ts
@@ -1,200 +1,259 @@
+import type { Schema } from "@dpkit/core"
 import { DataFrame } from "nodejs-polars"
 import { describe, expect, it } from "vitest"
 import { processTable } from "./process.js"
 
 describe("processTable", () => {
-  it.each([
-    {
-      description: "should work without schema",
-      table: DataFrame({
-        id: [1, 2],
-        name: ["english", "中文"],
-      }).lazy(),
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+  it("should work without schema", async () => {
+    const table = DataFrame({
+      id: [1, 2],
+      name: ["english", "中文"],
+    }).lazy()
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work with schema", async () => {
+    const table = DataFrame({
+      id: [1, 2],
+      name: ["english", "中文"],
+    }).lazy()
+
+    const schema: Schema = {
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should work with schema",
-      table: DataFrame({
-        id: [1, 2],
-        name: ["english", "中文"],
-      }).lazy(),
-      schema: {
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work with less fields in data", async () => {
+    const table = DataFrame({
+      id: [1, 2],
+      name: ["english", "中文"],
+    }).lazy()
+
+    const schema: Schema = {
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
+        { name: "other", type: "boolean" },
       ],
-    },
-    {
-      description: "should work with less fields in data",
-      table: DataFrame({
-        id: [1, 2],
-        name: ["english", "中文"],
-      }).lazy(),
-      schema: {
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-          { name: "other", type: "boolean" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english", other: null },
-        { id: 2, name: "中文", other: null },
+    }
+
+    const records = [
+      { id: 1, name: "english", other: null },
+      { id: 2, name: "中文", other: null },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work with more fields in data", async () => {
+    const table = DataFrame({
+      id: [1, 2],
+      name: ["english", "中文"],
+      other: [true, false],
+    }).lazy()
+
+    const schema: Schema = {
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should work with more fields in data",
-      table: DataFrame({
-        id: [1, 2],
-        name: ["english", "中文"],
-        other: [true, false],
-      }).lazy(),
-      schema: {
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work based on fields order", async () => {
+    const table = DataFrame({
+      field1: [1, 2],
+      field2: ["english", "中文"],
+    }).lazy()
+
+    const schema: Schema = {
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should work based on fields order",
-      table: DataFrame({
-        field1: [1, 2],
-        field2: ["english", "中文"],
-      }).lazy(),
-      schema: {
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work based on field names (equal)", async () => {
+    const table = DataFrame({
+      name: ["english", "中文"],
+      id: [1, 2],
+    }).lazy()
+
+    const schema: Schema = {
+      fieldsMatch: "equal",
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should work based on field names (equal)",
-      table: DataFrame({
-        name: ["english", "中文"],
-        id: [1, 2],
-      }).lazy(),
-      schema: {
-        fieldsMatch: "equal",
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work based on field names (subset)", async () => {
+    const table = DataFrame({
+      name: ["english", "中文"],
+      id: [1, 2],
+    }).lazy()
+
+    const schema: Schema = {
+      fieldsMatch: "subset",
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should work based on field names (subset)",
-      table: DataFrame({
-        name: ["english", "中文"],
-        id: [1, 2],
-      }).lazy(),
-      schema: {
-        fieldsMatch: "subset",
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work based on field names (superset)", async () => {
+    const table = DataFrame({
+      name: ["english", "中文"],
+      id: [1, 2],
+    }).lazy()
+
+    const schema: Schema = {
+      fieldsMatch: "superset",
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should work based on field names (superset)",
-      table: DataFrame({
-        name: ["english", "中文"],
-        id: [1, 2],
-      }).lazy(),
-      schema: {
-        fieldsMatch: "superset",
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should work based on field names (partial)", async () => {
+    const table = DataFrame({
+      name: ["english", "中文"],
+      id: [1, 2],
+    }).lazy()
+
+    const schema: Schema = {
+      fieldsMatch: "partial",
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should work based on field names (partial)",
-      table: DataFrame({
-        name: ["english", "中文"],
-        id: [1, 2],
-      }).lazy(),
-      schema: {
-        fieldsMatch: "partial",
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should parse string columns", async () => {
+    const table = DataFrame({
+      id: ["1", "2"],
+      name: ["english", "中文"],
+    }).lazy()
+
+    const schema: Schema = {
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "string" },
       ],
-    },
-    {
-      description: "should parse string columns",
-      table: DataFrame({
-        id: ["1", "2"],
-        name: ["english", "中文"],
-      }).lazy(),
-      schema: {
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "string" },
-        ],
-      },
-      records: [
-        { id: 1, name: "english" },
-        { id: 2, name: "中文" },
+    }
+
+    const records = [
+      { id: 1, name: "english" },
+      { id: 2, name: "中文" },
+    ]
+
+    const ldf = await processTable({ table, schema })
+    const df = await ldf.collect()
+    expect(df.toRecords()).toEqual(records)
+  })
+
+  it("should read type errors as nulls", async () => {
+    const table = DataFrame({
+      id: [1, 2],
+      name: ["english", "中文"],
+    }).lazy()
+
+    const schema: Schema = {
+      fields: [
+        { name: "id", type: "integer" },
+        { name: "name", type: "integer" },
       ],
-    },
-    {
-      description: "should read type errors as nulls",
-      table: DataFrame({
-        id: [1, 2],
-        name: ["english", "中文"],
-      }).lazy(),
-      schema: {
-        fields: [
-          { name: "id", type: "integer" },
-          { name: "name", type: "integer" },
-        ],
-      },
-      records: [
-        { id: 1, name: null },
-        { id: 2, name: null },
-      ],
-    },
-  ])("$description", async ({ table, schema, records }) => {
-    // @ts-ignore
+    }
+
+    const records = [
+      { id: 1, name: null },
+      { id: 2, name: null },
+    ]
+
     const ldf = await processTable({ table, schema })
     const df = await ldf.collect()
     expect(df.toRecords()).toEqual(records)
   })
 })
+
