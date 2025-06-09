569f46b5ee671a8ae03643afdeb00efa5b24c4e7
roll <eskarev@gmail.com>
2025-06-09 18:50:09 +0100

    Removed legacy checks

--- table/field/checks/required.ts
+++
@@ -1,7 +0,0 @@
-import type { Field } from "@dpkit/core"
-import type { Expr } from "nodejs-polars"
-import { lit } from "nodejs-polars"
-
-export function isCellRequiredError(field: Field, target: Expr) {
-  return field.constraints?.required ? target.isNull() : lit(false)
-}
--- table/field/checks/type.ts
+++
@@ -1,5 +0,0 @@
-import type { Expr } from "nodejs-polars"
-
-export function isCellTypeError(source: Expr, target: Expr) {
-  return source.isNotNull().and(target.isNull())
-}
