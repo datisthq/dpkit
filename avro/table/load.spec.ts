351d3e9a8e272d3563a8b65fef9ab729bb81f025
roll <eskarev@gmail.com>
2025-08-05 11:23:22 +0100

    Implemented avro loader

--- avro/table/load.spec.ts
+++ avro/table/load.spec.ts
@@ -1,18 +1,24 @@
-import { temporaryFileTask } from "tempy"
+import { getTempFilePath } from "@dpkit/file"
+import { DataFrame } from "nodejs-polars"
 import { describe, expect, it } from "vitest"
 import { loadAvroTable } from "./load.js"
 
 describe("loadAvroTable", () => {
   describe("file variations", () => {
     it("should load local file", async () => {
-      await temporaryFileTask(
-        async path => {
-          const table = await loadAvroTable({ path })
+      const path = getTempFilePath()
 
-          expect(table).toBeDefined()
-        },
-        { extension: "avro" },
-      )
+      DataFrame({
+        id: [1, 2],
+        name: ["english", "中文"],
+      }).writeAvro(path)
+
+      const table = await loadAvroTable({ path })
+
+      expect((await table.collect()).toRecords()).toEqual([
+        { id: "1", name: "english" },
+        { id: "2", name: "中文" },
+      ])
     })
   })
 })
--- avro/table/load.ts
+++ avro/table/load.ts
@@ -1,6 +1,12 @@
 import type { Resource } from "@dpkit/core"
+import { readAvro } from "nodejs-polars"
 import { DataFrame } from "nodejs-polars"
 
-export async function loadAvroTable(_resource: Partial<Resource>) {
-  return DataFrame().lazy()
+export async function loadAvroTable(resource: Partial<Resource>) {
+  if (typeof resource.path !== "string") {
+    return DataFrame().lazy()
+  }
+
+  const table = readAvro(resource.path).lazy()
+  return table
 }
