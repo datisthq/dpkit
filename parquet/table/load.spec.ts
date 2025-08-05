f634e3bf048e8a8a4bfe1e806e94f9d791136b40
roll <eskarev@gmail.com>
2025-08-05 11:49:54 +0100

    Implemented parquet load

--- parquet/table/load.spec.ts
+++ parquet/table/load.spec.ts
@@ -1,20 +1,64 @@
+import { getTempFilePath } from "@dpkit/file"
+import { useRecording } from "@dpkit/test"
 import { DataFrame } from "nodejs-polars"
-import { temporaryFileTask } from "tempy"
 import { describe, expect, it } from "vitest"
 import { loadParquetTable } from "./load.js"
 
 describe("loadParquetTable", () => {
+  useRecording()
+
   describe("file variations", () => {
     it("should load local file", async () => {
-      await temporaryFileTask(async path => {
-        DataFrame({
-          id: [1, 2],
-          name: ["english", "中文"],
-        }).writeParquet(path)
+      const path = getTempFilePath()
+      DataFrame({ id: [1, 2], name: ["english", "中文"] }).writeParquet(path)
 
-        const table = await loadParquetTable({ path })
-        expect(table).toBeDefined()
+      const table = await loadParquetTable({ path })
+      expect((await table.collect()).toRecords()).toEqual([
+        { id: 1, name: "english" },
+        { id: 2, name: "中文" },
+      ])
+    })
+
+    it("should load local file (multipart)", async () => {
+      const path1 = getTempFilePath()
+      const path2 = getTempFilePath()
+      DataFrame({ id: [1, 2], name: ["english", "中文"] }).writeParquet(path1)
+      DataFrame({ id: [1, 2], name: ["english", "中文"] }).writeParquet(path2)
+
+      const table = await loadParquetTable({ path: [path1, path2] })
+      expect((await table.collect()).toRecords()).toEqual([
+        { id: 1, name: "english" },
+        { id: 2, name: "中文" },
+        { id: 1, name: "english" },
+        { id: 2, name: "中文" },
+      ])
+    })
+
+    it("should load remote file", async () => {
+      const table = await loadParquetTable({
+        path: "https://github.com/frictionlessdata/frictionless-py/raw/refs/heads/main/data/table.parquet",
       })
+
+      expect((await table.collect()).toRecords()).toEqual([
+        { id: 1, name: "english" },
+        { id: 2, name: "中国人" },
+      ])
+    })
+
+    it("should load remote file (multipart)", async () => {
+      const table = await loadParquetTable({
+        path: [
+          "https://github.com/frictionlessdata/frictionless-py/raw/refs/heads/main/data/table.parquet",
+          "https://github.com/frictionlessdata/frictionless-py/raw/refs/heads/main/data/table.parquet",
+        ],
+      })
+
+      expect((await table.collect()).toRecords()).toEqual([
+        { id: 1, name: "english" },
+        { id: 2, name: "中国人" },
+        { id: 1, name: "english" },
+        { id: 2, name: "中国人" },
+      ])
     })
   })
 })
--- parquet/table/load.ts
+++ parquet/table/load.ts
@@ -1,6 +1,21 @@
 import type { Resource } from "@dpkit/core"
+import { prefetchFiles } from "@dpkit/file"
+import { concat } from "nodejs-polars"
 import { DataFrame } from "nodejs-polars"
+import { scanParquet } from "nodejs-polars"
 
-export async function loadParquetTable(_resource: Partial<Resource>) {
-  return DataFrame().lazy()
+export async function loadParquetTable(resource: Partial<Resource>) {
+  const [firstPath, ...restPaths] = await prefetchFiles(resource.path)
+
+  if (!firstPath) {
+    return DataFrame().lazy()
+  }
+
+  let table = scanParquet(firstPath)
+
+  if (restPaths.length) {
+    table = concat([table, ...restPaths.map(path => scanParquet(path))])
+  }
+
+  return table
 }
