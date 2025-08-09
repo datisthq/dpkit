768a97b1a1a657c29e1f8d4b18f8116a3f5c3089
roll <eskarev@gmail.com>
2025-08-09 14:21:57 +0100

    Renamed read to explore

--- cli/commands/table/explore.tsx
+++ cli/commands/table/explore.tsx
@@ -1,13 +1,13 @@
 import { Command } from "@oclif/core"
-import { type Dialect, readTable } from "dpkit"
+import { readTable } from "dpkit"
 import { render } from "ink"
 import React from "react"
 import { TableGrid } from "../../components/TableGrid.tsx"
 import * as options from "../../options/index.ts"
 import * as params from "../../params/index.ts"
 
-export default class ReadTable extends Command {
-  static override description = "Read a table from a local or remote path"
+export default class ExploreTable extends Command {
+  static override description = "Explore a table from a local or remote path"
 
   static override args = {
     path: params.requriedTablePath,
@@ -19,7 +19,7 @@ export default class ReadTable extends Command {
   }
 
   public async run() {
-    const { args, flags } = await this.parse(ReadTable)
+    const { args, flags } = await this.parse(ExploreTable)
 
     const dialect = options.createDialectFromFlags(flags)
     const table = await readTable({ path: args.path, dialect })
