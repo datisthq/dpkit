b5e448ae9a2e300a2931b55440fe282969539ae9
roll <eskarev@gmail.com>
2025-09-02 17:26:49 +0100

    Implemented loadTable from database

--- database/table/load.ts
+++ database/table/load.ts
@@ -1,43 +1,58 @@
 import { loadResourceDialect } from "@dpkit/core"
 import type { Resource } from "@dpkit/core"
-import type { DataRow, Table } from "@dpkit/table"
-import { getRecordsFromRows } from "@dpkit/table"
 import Database from "better-sqlite3"
+import { Kysely } from "kysely"
 import { MysqlDialect, PostgresDialect, SqliteDialect } from "kysely"
 import type { Dialect } from "kysely"
 import { createPool } from "mysql2"
+import { DataFrame } from "nodejs-polars"
 import { Pool } from "pg"
 
 export async function loadPostgresTable(resource: Partial<Resource>) {
   const url = typeof resource.path === "string" ? resource.path : undefined
 
   const pool = new Pool({ connectionString: url, max: 1 })
-  const dialect = new PostgresDialect({ pool })
+  const adapter = new PostgresDialect({ pool })
 
-  return await loadTable(resource, { dialect })
+  return await loadTable(resource, { adapter })
 }
 
 export async function loadMysqlTable(resource: Partial<Resource>) {
   const url = typeof resource.path === "string" ? resource.path : undefined
 
   const pool = createPool({ uri: url, connectionLimit: 1 })
-  const dialect = new MysqlDialect({ pool })
+  const adapter = new MysqlDialect({ pool })
 
-  return await loadTable(resource, { dialect })
+  return await loadTable(resource, { adapter })
 }
 
 export async function loadSqliteTable(resource: Partial<Resource>) {
   const url = typeof resource.path === "string" ? resource.path : undefined
 
   const database = new Database(url)
-  const dialect = new SqliteDialect({ database })
+  const adapter = new SqliteDialect({ database })
 
-  return await loadTable(resource, { dialect })
+  return await loadTable(resource, { adapter })
 }
 
 export async function loadTable(
   resource: Partial<Resource>,
   options: {
-    dialect: Dialect
+    adapter: Dialect
   },
-) {}
+) {
+  const { adapter } = options
+  const db = new Kysely({ dialect: adapter })
+
+  const dialect = await loadResourceDialect(resource.dialect)
+  if (!dialect?.table) {
+    throw new Error("Table name is not defined")
+  }
+
+  // @ts-ignore
+  const records = await db.selectFrom(dialect.table).selectAll().execute()
+  db.destroy()
+
+  const table = DataFrame(records).lazy()
+  return table
+}
