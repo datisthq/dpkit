6d714a1ef36f31a839d75780817cf7fa2e0b16df
roll <eskarev@gmail.com>
2025-09-04 15:46:06 +0100

    Full-functional parquet table load

--- parquet/table/load.ts
+++ parquet/table/load.ts
@@ -1,10 +1,16 @@
 import type { Resource } from "@dpkit/core"
+import { loadResourceSchema } from "@dpkit/core"
 import { prefetchFiles } from "@dpkit/file"
+import type { LoadTableOptions } from "@dpkit/table"
+import { inferSchema, processTable } from "@dpkit/table"
 import { concat } from "nodejs-polars"
 import { DataFrame } from "nodejs-polars"
 import { scanParquet } from "nodejs-polars"
 
-export async function loadParquetTable(resource: Partial<Resource>) {
+export async function loadParquetTable(
+  resource: Partial<Resource>,
+  options?: LoadTableOptions,
+) {
   const [firstPath, ...restPaths] = await prefetchFiles(resource.path)
   if (!firstPath) {
     return DataFrame().lazy()
@@ -13,6 +19,15 @@ export async function loadParquetTable(resource: Partial<Resource>) {
   let table = scanParquet(firstPath)
   if (restPaths.length) {
     table = concat([table, ...restPaths.map(path => scanParquet(path))])
+  }
+
+  let schema = await loadResourceSchema(resource.schema)
+  if (!schema && !options?.noInfer) {
+    schema = await inferSchema(table)
+  }
+
+  if (schema) {
+    table = await processTable(table, { schema })
   }
 
   return table
