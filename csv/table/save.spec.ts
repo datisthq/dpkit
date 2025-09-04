bf0616c763719a228a8bc396a3ef516ff125539e
roll <eskarev@gmail.com>
2025-09-04 11:24:53 +0100

    Added csv save data types test

--- csv/table/save.spec.ts
+++ csv/table/save.spec.ts
@@ -1,16 +1,18 @@
 import { readFile } from "node:fs/promises"
 import { getTempFilePath } from "@dpkit/file"
-import { DataFrame } from "nodejs-polars"
+import * as pl from "nodejs-polars"
 import { describe, expect, it } from "vitest"
 import { saveCsvTable } from "./save.ts"
 
 describe("saveCsvTable", () => {
   it("should save table to file", async () => {
     const path = getTempFilePath()
-    const table = DataFrame({
-      id: [1.0, 2.0, 3.0],
-      name: ["Alice", "Bob", "Charlie"],
-    }).lazy()
+    const table = pl
+      .DataFrame({
+        id: [1.0, 2.0, 3.0],
+        name: ["Alice", "Bob", "Charlie"],
+      })
+      .lazy()
 
     await saveCsvTable(table, { path })
 
@@ -20,10 +22,12 @@ describe("saveCsvTable", () => {
 
   it("should save with custom delimiter", async () => {
     const path = getTempFilePath()
-    const table = DataFrame({
-      id: [1.0, 2.0, 3.0],
-      name: ["Alice", "Bob", "Charlie"],
-    }).lazy()
+    const table = pl
+      .DataFrame({
+        id: [1.0, 2.0, 3.0],
+        name: ["Alice", "Bob", "Charlie"],
+      })
+      .lazy()
 
     await saveCsvTable(table, {
       path,
@@ -36,10 +40,12 @@ describe("saveCsvTable", () => {
 
   it("should save without header", async () => {
     const path = getTempFilePath()
-    const table = DataFrame({
-      id: [1.0, 2.0, 3.0],
-      name: ["Alice", "Bob", "Charlie"],
-    }).lazy()
+    const table = pl
+      .DataFrame({
+        id: [1.0, 2.0, 3.0],
+        name: ["Alice", "Bob", "Charlie"],
+      })
+      .lazy()
 
     await saveCsvTable(table, {
       path,
@@ -52,10 +58,12 @@ describe("saveCsvTable", () => {
 
   it("should save with custom quote char", async () => {
     const path = getTempFilePath()
-    const table = DataFrame({
-      id: [1.0, 2.0, 3.0],
-      name: ["Alice,Smith", "Bob,Jones", "Charlie,Brown"],
-    }).lazy()
+    const table = pl
+      .DataFrame({
+        id: [1.0, 2.0, 3.0],
+        name: ["Alice,Smith", "Bob,Jones", "Charlie,Brown"],
+      })
+      .lazy()
 
     await saveCsvTable(table, {
       path,
@@ -66,5 +74,62 @@ describe("saveCsvTable", () => {
     expect(content).toEqual(
       "id,name\n1.0,'Alice,Smith'\n2.0,'Bob,Jones'\n3.0,'Charlie,Brown'\n",
     )
+  })
+
+  it("should save various data types", async () => {
+    const path = getTempFilePath()
+
+    const table = pl
+      .DataFrame([
+        pl.Series("string", ["string"], pl.Utf8),
+        pl.Series("integer", [1], pl.Int32),
+        pl.Series("number", [1.1], pl.Float64),
+        pl.Series("boolean", [true], pl.Bool),
+        //pl.Series("object", [{ value: 1 }]),
+        //pl.Series("array", [[1, 2, 3]], pl.List(pl.Int32)),
+        //pl.Series("list", [[1, 2, 3]], pl.List(pl.Int32)),
+        pl.Series("datetime", [new Date(Date.UTC(2025, 0, 1))], pl.Datetime),
+        pl.Series("date", [new Date(Date.UTC(2025, 0, 1))], pl.Date),
+        pl.Series("time", [new Date(Date.UTC(2025, 0, 1))], pl.Time),
+        pl.Series("year", [2025], pl.Int32),
+        //pl.Series("yearmonth", [[2025, 1]], pl.List(pl.Int32)),
+        pl.Series("duration", ["P23DT23H"], pl.Utf8),
+        //pl.Series("geopoint", [[40.00, 50.00]], pl.List(pl.Float32)),
+        //pl.Series("geojson", [{ value: 1 }]),
+      ])
+      .lazy()
+
+    await saveCsvTable(table, { path, dialect: { delimiter: ";" } })
+    const content = await readFile(path, "utf-8")
+    const [head, body] = content.split("\n")
+
+    const labels = head?.split(";") ?? []
+    const cells = body?.split(";") ?? []
+
+    const records = Object.fromEntries(
+      labels.map((label, index) => [label, cells[index]]),
+    )
+
+    expect(records).toEqual({
+      string: "string",
+      integer: "1",
+      number: "1.1",
+      boolean: "true",
+      // TODO: fix
+      //object: "{\"value\":1}",
+      //array: "[1,2,3]",
+      //list: "[1,2,3]",
+      // TODO: fix
+      datetime: "2025-01-01T00:00:00.000",
+      date: "2025-01-01",
+      // TODO: fix
+      time: "2025-01-01T00:00:00.000",
+      year: "2025",
+      // TODO: fix
+      //yearmonth: "[2025,1]",
+      duration: "P23DT23H",
+      // geopoint: "[40.00,50.00]",
+      //geojson: "{\"value\":1}",
+    })
   })
 })
