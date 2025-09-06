89de66883f2a0ee3d8a4cb8ca917213a6d1b500c
roll <eskarev@gmail.com>
2025-09-06 10:43:39 +0100

    Split json/jsonl specs

--- json/table/save.json.spec.ts
+++ json/table/save.json.spec.ts
@@ -1,5 +1,4 @@
 import { readFile } from "node:fs/promises"
-import { format } from "node:path"
 import { getTempFilePath } from "@dpkit/file"
 import { readRecords } from "nodejs-polars"
 import { describe, expect, it } from "vitest"
@@ -9,7 +8,7 @@ const row1 = { id: 1, name: "english" }
 const row2 = { id: 2, name: "中文" }
 const table = readRecords([row1, row2]).lazy()
 
-describe("saveJsonTable (json)", () => {
+describe("saveJsonTable", () => {
   it("should save table to file", async () => {
     const path = getTempFilePath()
     await saveJsonTable(table, { path, format: "json" })
@@ -62,68 +61,6 @@ describe("saveJsonTable (json)", () => {
         null,
         2,
       ),
-    )
-  })
-})
-
-describe("saveJsonTable (jsonl)", () => {
-  it("should save table to file", async () => {
-    const path = getTempFilePath()
-
-    await saveJsonTable(table, { path, format: "jsonl" })
-
-    const content = await readFile(path, "utf-8")
-    expect(content).toEqual(
-      [JSON.stringify(row1), JSON.stringify(row2)].join("\n"),
-    )
-  })
-
-  it("should handle item keys", async () => {
-    const path = getTempFilePath()
-    await saveJsonTable(table, {
-      path,
-      format: "jsonl",
-      dialect: { itemKeys: ["name"] },
-    })
-
-    const content = await readFile(path, "utf-8")
-    expect(content).toEqual(
-      [
-        JSON.stringify({ name: row1.name }),
-        JSON.stringify({ name: row2.name }),
-      ].join("\n"),
-    )
-  })
-
-  it("should handle item type (array)", async () => {
-    const path = getTempFilePath()
-    await saveJsonTable(table, {
-      path,
-      format: "jsonl",
-      dialect: { itemType: "array" },
-    })
-
-    const content = await readFile(path, "utf-8")
-    expect(content).toEqual(
-      [
-        JSON.stringify(Object.keys(row1)),
-        JSON.stringify(Object.values(row1)),
-        JSON.stringify(Object.values(row2)),
-      ].join("\n"),
-    )
-  })
-
-  it("should handle item type (object)", async () => {
-    const path = getTempFilePath()
-    await saveJsonTable(table, {
-      path,
-      format: "jsonl",
-      dialect: { itemType: "object" },
-    })
-
-    const content = await readFile(path, "utf-8")
-    expect(content).toEqual(
-      [JSON.stringify(row1), JSON.stringify(row2)].join("\n"),
     )
   })
 })
---
+++ json/table/save.jsonl.spec.ts
@@ -0,0 +1,71 @@
+import { readFile } from "node:fs/promises"
+import { getTempFilePath } from "@dpkit/file"
+import { readRecords } from "nodejs-polars"
+import { describe, expect, it } from "vitest"
+import { saveJsonTable } from "./save.ts"
+
+const row1 = { id: 1, name: "english" }
+const row2 = { id: 2, name: "中文" }
+const table = readRecords([row1, row2]).lazy()
+
+describe("saveJsonTable", () => {
+  it("should save table to file", async () => {
+    const path = getTempFilePath()
+
+    await saveJsonTable(table, { path, format: "jsonl" })
+
+    const content = await readFile(path, "utf-8")
+    expect(content).toEqual(
+      [JSON.stringify(row1), JSON.stringify(row2)].join("\n"),
+    )
+  })
+
+  it("should handle item keys", async () => {
+    const path = getTempFilePath()
+    await saveJsonTable(table, {
+      path,
+      format: "jsonl",
+      dialect: { itemKeys: ["name"] },
+    })
+
+    const content = await readFile(path, "utf-8")
+    expect(content).toEqual(
+      [
+        JSON.stringify({ name: row1.name }),
+        JSON.stringify({ name: row2.name }),
+      ].join("\n"),
+    )
+  })
+
+  it("should handle item type (array)", async () => {
+    const path = getTempFilePath()
+    await saveJsonTable(table, {
+      path,
+      format: "jsonl",
+      dialect: { itemType: "array" },
+    })
+
+    const content = await readFile(path, "utf-8")
+    expect(content).toEqual(
+      [
+        JSON.stringify(Object.keys(row1)),
+        JSON.stringify(Object.values(row1)),
+        JSON.stringify(Object.values(row2)),
+      ].join("\n"),
+    )
+  })
+
+  it("should handle item type (object)", async () => {
+    const path = getTempFilePath()
+    await saveJsonTable(table, {
+      path,
+      format: "jsonl",
+      dialect: { itemType: "object" },
+    })
+
+    const content = await readFile(path, "utf-8")
+    expect(content).toEqual(
+      [JSON.stringify(row1), JSON.stringify(row2)].join("\n"),
+    )
+  })
+})
