6af5ed61cdea578646e2127edbd3475119a86f07
roll <eskarev@gmail.com>
2025-09-09 11:59:51 +0100

    Moved infer test to drivers

--- database/drivers/sqlite.spec.ts
+++ database/drivers/sqlite.spec.ts
@@ -2,6 +2,7 @@ import { getTempFilePath } from "@dpkit/file"
 import { useRecording } from "@dpkit/test"
 import * as pl from "nodejs-polars"
 import { describe, expect, it } from "vitest"
+import { inferDatabaseSchema } from "../schema/index.ts"
 import { loadDatabaseTable, saveDatabaseTable } from "../table/index.ts"
 
 useRecording()
@@ -11,6 +12,33 @@ const record1 = { id: 1, name: "english" }
 const record2 = { id: 2, name: "中文" }
 
 describe("SqliteDriver", () => {
+  it("should infer schema", async () => {
+    const path = getTempFilePath()
+
+    const source = pl
+      .DataFrame([
+        pl.Series("string", ["string"], pl.Utf8),
+        pl.Series("integer", [1], pl.Int32),
+        pl.Series("number", [1.1], pl.Float64),
+      ])
+      .lazy()
+
+    await saveDatabaseTable(source, { path, dialect, format: "sqlite" })
+    const schema = await inferDatabaseSchema({
+      path,
+      dialect,
+      format: "sqlite",
+    })
+
+    expect(schema).toEqual({
+      fields: [
+        { name: "string", type: "string" },
+        { name: "integer", type: "integer" },
+        { name: "number", type: "number" },
+      ],
+    })
+  })
+
   it("should save/load table", async () => {
     const path = getTempFilePath()
 
