84ba0a345f636817f7936af4c5d9fb1154419ac6
roll <eskarev@gmail.com>
2025-09-11 13:13:03 +0100

    Added parquet denormalization

--- parquet/table/save.ts
+++ parquet/table/save.ts
@@ -1,4 +1,5 @@
 import { assertLocalPathVacant } from "@dpkit/file"
+import { denormalizeTable, inferTableSchema } from "@dpkit/table"
 import type { SaveTableOptions, Table } from "@dpkit/table"
 
 export async function saveParquetTable(
@@ -10,6 +11,23 @@ export async function saveParquetTable(
   if (!overwrite) {
     await assertLocalPathVacant(path)
   }
+
+  const schema = await inferTableSchema(table, {
+    ...options,
+    keepStrings: true,
+  })
+
+  table = await denormalizeTable(table, schema, {
+    keepTypes: [
+      "boolean",
+      "datetime",
+      "integer",
+      "list",
+      "number",
+      "string",
+      "year",
+    ],
+  })
 
   await table
     .sinkParquet(path, {
