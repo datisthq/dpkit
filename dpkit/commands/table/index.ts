789f39c54b803ba7e6020dbfbf43334de79a3256
roll <eskarev@gmail.com>
2025-09-15 10:05:09 +0100

    Removed table.query command

--- dpkit/commands/table/index.ts
+++ dpkit/commands/table/index.ts
@@ -3,7 +3,6 @@ import { helpConfiguration } from "../../helpers/help.ts"
 import { convertTableCommand } from "./convert.tsx"
 import { errorsTableCommand } from "./errors.tsx"
 import { exploreTableCommand } from "./explore.tsx"
-import { queryTableCommand } from "./query.tsx"
 import { scriptTableCommand } from "./script.tsx"
 import { statsTableCommand } from "./stats.tsx"
 import { validateTableCommand } from "./validate.tsx"
@@ -15,7 +14,6 @@ export const tableCommand = new Command("table")
   .addCommand(convertTableCommand)
   .addCommand(errorsTableCommand)
   .addCommand(exploreTableCommand)
-  .addCommand(queryTableCommand)
   .addCommand(scriptTableCommand)
   .addCommand(statsTableCommand)
   .addCommand(validateTableCommand)
