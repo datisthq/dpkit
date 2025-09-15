57253a9cd810c7245682e44313d812f9b30ac0fe
roll <eskarev@gmail.com>
2025-09-15 14:38:56 +0100

    Added options to inferResource/Package

--- all/package/infer.ts
+++ all/package/infer.ts
@@ -1,4 +1,6 @@
 import type { Package, Resource } from "@dpkit/core"
+import type { InferDialectOptions } from "@dpkit/table"
+import type { InferSchemaOptions } from "@dpkit/table"
 import { inferResource } from "../resource/index.ts"
 
 // TODO: Move PartialPackage/Resource to @dpkit/core?
@@ -7,12 +9,15 @@ interface PartialPackage extends Omit<Package, "resources"> {
   resources: Partial<Resource>[]
 }
 
-export async function inferPackage(dataPackage: PartialPackage) {
+export async function inferPackage(
+  dataPackage: PartialPackage,
+  options?: InferDialectOptions & InferSchemaOptions,
+) {
   const result = {
     ...dataPackage,
     resources: await Promise.all(
       dataPackage.resources.map(async resource => {
-        return await inferResource(resource)
+        return await inferResource(resource, options)
       }),
     ),
   }
