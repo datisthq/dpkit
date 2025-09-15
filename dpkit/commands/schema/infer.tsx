92a23078c38c689d0a25b6be6f1095c94b0a5b64
roll <eskarev@gmail.com>
2025-09-15 08:58:16 +0100

    Added dialect/schema options to schema infer command

--- dpkit/commands/schema/infer.tsx
+++ dpkit/commands/schema/infer.tsx
@@ -2,6 +2,7 @@ import { inferSchemaFromTable, loadTable } from "@dpkit/all"
 import { Command } from "commander"
 import React from "react"
 import { SchemaGrid } from "../../components/SchemaGrid.tsx"
+import { createDialectFromOptions } from "../../helpers/dialect.ts"
 import { helpConfiguration } from "../../helpers/help.ts"
 import { isEmptyObject } from "../../helpers/object.ts"
 import { selectResource } from "../../helpers/resource.ts"
@@ -18,6 +19,44 @@ export const inferSchemaCommand = new Command("infer")
   .addOption(params.json)
   .addOption(params.debug)
 
+  .optionsGroup("Table Dialect")
+  .addOption(params.delimiter)
+  .addOption(params.header)
+  .addOption(params.headerRows)
+  .addOption(params.headerJoin)
+  .addOption(params.commentRows)
+  .addOption(params.commentChar)
+  .addOption(params.quoteChar)
+  .addOption(params.doubleQuote)
+  .addOption(params.escapeChar)
+  .addOption(params.nullSequence)
+  .addOption(params.skipInitialSpace)
+  .addOption(params.property)
+  .addOption(params.itemType)
+  .addOption(params.itemKeys)
+  .addOption(params.sheetNumber)
+  .addOption(params.sheetName)
+  .addOption(params.table)
+
+  .optionsGroup("Table Schema")
+  .addOption(params.fieldNames)
+  .addOption(params.fieldTypes)
+  .addOption(params.missingValues)
+  .addOption(params.stringFormat)
+  .addOption(params.decimalChar)
+  .addOption(params.groupChar)
+  .addOption(params.bareNumber)
+  .addOption(params.trueValues)
+  .addOption(params.falseValues)
+  .addOption(params.datetimeFormat)
+  .addOption(params.dateFormat)
+  .addOption(params.timeFormat)
+  .addOption(params.arrayType)
+  .addOption(params.listDelimiter)
+  .addOption(params.listItemType)
+  .addOption(params.geopointFormat)
+  .addOption(params.geojsonFormat)
+
   .action(async (path, options) => {
     const session = Session.create({
       title: "Infer schema",
@@ -25,7 +64,9 @@ export const inferSchemaCommand = new Command("infer")
       debug: options.debug,
     })
 
-    const resource = path ? { path } : await selectResource(session, options)
+    const resource = path
+      ? { path, dialect: createDialectFromOptions(options) }
+      : await selectResource(session, options)
 
     const table = await session.task(
       "Loading table",
@@ -34,7 +75,9 @@ export const inferSchemaCommand = new Command("infer")
 
     const inferredSchema = await session.task(
       "Inferring schema",
-      inferSchemaFromTable(table),
+      // TODO: Fix typing
+      // @ts-ignore
+      inferSchemaFromTable(table, options),
     )
 
     if (isEmptyObject(inferredSchema)) {
