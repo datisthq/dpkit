4c16c52cfc284018bf2b4ef2685e6d53f2455d32
roll <eskarev@gmail.com>
2025-09-16 09:35:38 +0100

    Properly support withRemote

--- database/package/save.ts
+++ database/package/save.ts
@@ -1,20 +1,27 @@
 import type { Package } from "@dpkit/core"
 import { loadResourceSchema } from "@dpkit/core"
+import { isRemoteResource } from "@dpkit/core"
+import type { SavePackageOptions } from "@dpkit/core"
 import type { TablePlugin } from "@dpkit/table"
 import type { DatabaseFormat } from "../resource/index.ts"
 import { saveDatabaseTable } from "../table/index.ts"
 
 export async function savePackageToDatabase(
   dataPackage: Package,
-  options: {
-    target: string
+  options: SavePackageOptions & {
     format: DatabaseFormat
     plugins?: TablePlugin[]
   },
 ) {
   for (const resource of dataPackage.resources) {
     for (const plugin of options.plugins ?? []) {
+      const isRemote = isRemoteResource(resource)
+      if (isRemote && !options.withRemote) {
+        continue
+      }
+
       const table = await plugin.loadTable?.(resource)
+
       if (table) {
         const dialect = { table: resource.name }
         const schema = await loadResourceSchema(resource.schema)
