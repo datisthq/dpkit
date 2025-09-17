e56ddc860b18abde94336dd9269e52eaf8ddd84c
roll <eskarev@gmail.com>
2025-09-17 10:50:52 +0100

    Removed node experimental warnings

--- dpkit/entrypoints/dev.ts
+++ dpkit/entrypoints/dev.ts
@@ -1,3 +1,11 @@
 #!dpkit/node_modules/.bin/tsx
 
+process.removeAllListeners("warning")
+process.on("warning", warning => {
+  if (warning.name === "ExperimentalWarning") {
+    return
+  }
+  console.warn(warning)
+})
+
 await import("../main.ts")
