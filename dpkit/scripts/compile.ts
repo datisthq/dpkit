5659082830c4fc32c75ab3311047bea79f084fa5
roll <eskarev@gmail.com>
2025-09-19 17:14:53 +0100

    Setup proper binary management

--- dpkit/scripts/compile.ts
+++ dpkit/scripts/compile.ts
@@ -25,15 +25,51 @@ pnpm deploy compile
 --config.node-linker=hoisted
 `
 
+// Normalize package.json
+
+await $compile`
+sed -i /workspace:/d package.json
+`
+
+// Uninstall binaries
+
+const binaries = [
+  { polars: "nodejs-polars-linux-x64-gnu", libsql: "@libsql/linux-x64-gnu" },
+  { polars: "nodejs-polars-linux-x64-musl", libsql: "@libsql/linux-x64-musl" },
+]
+
+for (const binary of binaries) {
+  await $compile`npm uninstall ${binary.polars}`
+  await $compile`npm uninstall ${binary.libsql}`
+}
+
 // Compile executable
 
 const targets = [
-  { name: "bun-linux-x64", arch: "x86_64-unknown-linux" },
-  { name: "bun-darwin-x64", arch: "x86_64-apple-darwin" },
-  { name: "bun-windows-x64", arch: "x86_64-pc-windows" },
+  {
+    name: "bun-linux-x64",
+    arch: "x86_64-unknown-linux",
+    polars: "nodejs-polars-linux-x64-musl",
+    libsql: "@libsql/linux-x64-gnu",
+  },
+  {
+    name: "bun-darwin-x64",
+    arch: "x86_64-apple-darwin",
+    polars: "nodejs-polars-darwin-x64",
+    libsql: "@libsql/darwin-x64",
+  },
+  {
+    name: "bun-windows-x64",
+    arch: "x86_64-pc-windows",
+    polars: "nodejs-polars-win32-x64-msvc",
+    libsql: "@libsql/win32-x64-msvc",
+  },
 ]
 
 for (const target of targets) {
+  await $compile`npm install ${target.polars} --force`
+  await $compile`npm install ${target.libsql} --force`
+
   await $compile`
   bun build main.ts
   --compile
@@ -42,6 +78,6 @@ for (const target of targets) {
   `
 }
 
-// Clean pnpm bug (it creates an unwanted dpkit folder)
+// Clean artifacts (pnpm creates an unwanted dpkit folder)
 
 await $root`rm -r dpkit`
