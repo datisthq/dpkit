44fa07d14fd6066bc00468b124e1ff23bb6f254e
roll <eskarev@gmail.com>
2025-09-20 10:39:33 +0100

    Fixed libsql

--- dpkit/scripts/compile.ts
+++ dpkit/scripts/compile.ts
@@ -72,8 +72,9 @@ for (const target of targets) {
     await $compile`rm ${pack.stdout}`
   }
 
-  await $compile`
-  bun build main.ts
+  // TODO: Remove shell and libsql inclusion when libsql@0.6 is released (napi-rs based)
+  await $compile({ shell: true })`
+  bun build main.ts node_modules/@libsql/**/*
   --compile
   --outfile build/dp-${metadata.version}-${target.arch}/dp
   --target ${target.name}
