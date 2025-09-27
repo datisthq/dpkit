8810c1eb12d0e94f13de0b9aaf4d4df11e089e02
roll <eskarev@gmail.com>
2025-09-27 11:36:14 +0100

    Fixed error grid

--- dpkit/components/ErrorGrid.tsx
+++ dpkit/components/ErrorGrid.tsx
@@ -7,7 +7,14 @@ export function ErrorGrid(props: {
   errors: (TableError | MetadataError | FileError)[]
   quit?: boolean
 }) {
-  const table = DataFrame(props.errors).lazy()
+  // TODO: Property process errors
+  const errors = props.errors.map(error => ({
+    ...error,
+    // @ts-ignore
+    params: error.params ? error.params.toString() : undefined,
+  }))
+
+  const table = DataFrame(errors).lazy()
 
   return <TableGrid borderColor="red" table={table} quit={props.quit} />
 }
