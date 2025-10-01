8445350b4b429852be71869732bc64b7f1b95366
roll <eskarev@gmail.com>
2025-10-01 10:50:25 +0100

    Implemented normalize

--- jsonschema/schema/denormalize.ts
+++ jsonschema/schema/denormalize.ts
@@ -1,6 +1,6 @@
 import type { Schema } from "@dpkit/core"
 import type { JSONSchema7 } from "json-schema"
 
-export function denormalizeJsonSchema(schema: Schema): JSONSchema7 {
+export function denormalizeJsonSchema(_schema: Schema): JSONSchema7 {
   return {}
 }
