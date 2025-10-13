b45e34367d9942ab921463b231f3149eeb1b7aee
roll <eskarev@gmail.com>
2025-10-13 13:12:08 +0100

    test: Added zip testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
         "**/compile/**",
         "**/coverage/**",
         "**/scripts/**",
+        "**/entrypoints/**",
         "docs/**",
         "site/**",
         "examples/**",
