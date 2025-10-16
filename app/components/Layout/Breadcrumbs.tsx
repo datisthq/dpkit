dc1a5a637e231a06e6b47198ccfaf8ba3d10db3b
roll <eskarev@gmail.com>
2025-10-16 16:12:35 +0100

    Updated breadcrumbs

--- app/components/Layout/Breadcrumbs.tsx
+++ app/components/Layout/Breadcrumbs.tsx
@@ -1,5 +1,7 @@
 import { Anchor, Breadcrumbs as MantineBreadcrumbs } from "@mantine/core"
+import { Text } from "@mantine/core"
 import { useTranslation } from "react-i18next"
+import { TypeAnimation } from "react-type-animation"
 import { Link } from "#components/Link/index.ts"
 import { usePayload } from "#components/System/index.ts"
 import { useMakeLink } from "#components/System/index.ts"
@@ -9,18 +11,20 @@ export function Breadcrumbs() {
   const payload = usePayload()
   const makeLink = useMakeLink()
 
-  if (payload.page.pageId === "home") {
-    return null
-  }
-
   return (
     <MantineBreadcrumbs>
       <Anchor to={makeLink({ pageId: "home" })} component={Link}>
         {t("Tools")}
       </Anchor>
-      <Anchor to={makeLink({ pageId: "home" })} component={Link}>
-        {payload.page.title[payload.language.languageId]}
-      </Anchor>
+      {payload.page.pageId === "home" ? (
+        <Text color="dimmed">
+          <TypeAnimation sequence={[t("Select from the list below")]} />
+        </Text>
+      ) : (
+        <Anchor to={makeLink({ pageId: payload.page.pageId })} component={Link}>
+          {payload.page.title[payload.language.languageId]}
+        </Anchor>
+      )}
     </MantineBreadcrumbs>
   )
 }
