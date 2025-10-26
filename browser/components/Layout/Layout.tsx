3c43f8ab49dd23650c6c3cf3eb381d37679878d1
roll <eskarev@gmail.com>
2025-10-26 14:11:06 +0000

    chore: Use fixed header on mobile

--- browser/components/Layout/Layout.tsx
+++ browser/components/Layout/Layout.tsx
@@ -3,13 +3,14 @@ import { Banner } from "./Banner.tsx"
 import { Content } from "./Content.tsx"
 import { Footer } from "./Footer.tsx"
 import { Header } from "./Header.tsx"
+import classes from "./Layout.module.css"
 import { Meta } from "./Meta.tsx"
 
 export function Layout(props: {
   children?: React.ReactNode
 }) {
   return (
-    <Stack gap={40}>
+    <Stack gap={40} className={classes.root}>
       <Meta />
       <Box>
         <Header />
