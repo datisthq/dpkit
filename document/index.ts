4d66f0c1c8d6c017bf8e8161b96322f19920541d
roll <eskarev@gmail.com>
2025-11-06 11:27:20 +0000

    Explicit document exports

--- document/index.ts
+++ document/index.ts
@@ -1,2 +1,6 @@
-export * from "./document/index.ts"
-export * from "./schema/index.ts"
+export type { Document } from "./document/index.ts"
+export type { JsonDocument } from "./document/index.ts"
+
+export { convertSchemaToHtml } from "./schema/index.tsx"
+export { convertSchemaToMarkdown } from "./schema/index.ts"
+export { validateDocument } from "./document/index.ts"
