216c39543d0c9a368483f0642e7504ac4d7754f3
roll <eskarev@gmail.com>
2025-11-10 10:45:56 +0000

    Upgraded react-router

--- browser/vite.config.ts
+++ browser/vite.config.ts
@@ -2,7 +2,7 @@ import { cloudflare } from "@cloudflare/vite-plugin"
 import { lingui } from "@lingui/vite-plugin"
 import { reactRouter } from "@react-router/dev/vite"
 import { defineConfig } from "vite"
-import macrosPlugin from "vite-plugin-babel-macros"
+import babel from "vite-plugin-babel"
 import devtoolsJson from "vite-plugin-devtools-json"
 import svgr from "vite-plugin-svgr"
 
@@ -12,7 +12,17 @@ export default defineConfig({
     // @ts-ignore
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     devtoolsJson(),
-    macrosPlugin(),
+    {
+      // @ts-ignore
+      ...babel({
+        filter: /\.(ts|tsx)$/,
+        babelConfig: {
+          presets: ["@babel/preset-typescript"],
+          plugins: ["babel-plugin-macros", "@lingui/babel-plugin-lingui-macro"],
+        },
+      }),
+      apply: "build",
+    },
     lingui(),
     reactRouter(),
     svgr(),
